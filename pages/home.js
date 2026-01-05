a2ff13ecc340dbf2b58c1d6dd68073b99d3672ef
YJac16 <yaseenjacobs97@gmail.com>
2026-01-05 12:43:38 +0200

    Use logo with name on home page and make it smaller

--- pages/home.js
+++ pages/home.js
@@ -6,7 +6,7 @@ export function renderHome(container) {
   container.innerHTML = `
     <div class="content">
       <section class="hero">
-        <img src="/Athariq Logo Vector.png" alt="Āthariq" class="hero-logo-img">
+        <img src="/Athariq Logo.png" alt="Āthariq" class="hero-logo-img">
         <p class="hero-tagline">
           Interactive stories where every choice leaves a trace.
         </p>
